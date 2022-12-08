b3dea03b9b648d7cad1ff1a2cb47ba8dec214d09
wangxiaoxiao <wxx@tokeninsight.com>
2022-12-08 16:45:17 +0800

    template

--- src/api/index.ts
+++ src/api/index.ts
@@ -3,12 +3,16 @@
 import axios from '@/request'
 import type { SearchType } from './type'
 
+// 'http://43.206.26.117:32261'   开发模式服务
+let api = import.meta.env.MODE == 'development' ? 'https://widget.tokeninsight.com' : 'https://widget.tokeninsight.com'
+
+
 export const searchPost = (params: SearchType) => {
-  return axios.post('/apiv2/search/searchAllList', params)
+  return axios.post(api +'/apiv2/search/searchAllList', params)
 }
 export const getInvestorList = () => {
-  return axios.get('/api/user/dd/selectInvestorList')
+  return axios.get(api +'/api/user/dd/selectInvestorList')
 }
 export const saveAllInformation = (params: any, config?: any) => {
-  return axios.post('/api/listing/saveAllInformation', params, config)
+  return axios.post(api +'/api/listing/saveAllInformation', params, config)
 }
