e97df7c3308ee160e63c93a6722a3ea3cf7e6ec9
wangxiaoxiao <wxx90n@gmail.com>
2023-07-27 14:34:53 +0800

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { defineCustomElement } from './defineCustomElementWithStyles'
-import App from './App.ce.vue' // 引入
+import App from './App.ce.vue' // 引入App组件
 
 const AppElement = defineCustomElement(App) // 生成 HTMLElement 构造函数
 
